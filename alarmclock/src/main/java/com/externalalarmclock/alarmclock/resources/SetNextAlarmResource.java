08b303f7566d75eb4d48646fe76d6929601115e3
Michiel Leegwater <michiel@leegwater.info>
2017-07-13 21:09:15 +0200

    Added simple test using WS2812 library.

--- alarmclock/src/main/java/com/externalalarmclock/alarmclock/resources/SetNextAlarmResource.java
+++ alarmclock/src/main/java/com/externalalarmclock/alarmclock/resources/SetNextAlarmResource.java
@@ -1,5 +1,7 @@
 package com.externalalarmclock.alarmclock.resources;
 
+import java.awt.Color;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import com.codahale.metrics.annotation.Timed;
 import com.externalalarmclock.pojo.ExternalAlarm;
 
+import de.pi3g.pi.ws2812.WS2812;
+
 @Path("/setNextAlarm")
 @Produces(MediaType.APPLICATION_JSON)
 public class SetNextAlarmResource {
@@ -21,5 +25,15 @@ public class SetNextAlarmResource {
 	public void setNextAlarm(ExternalAlarm alarm) {
 		LOG.info("Next alarm set to: {}, vibrate: {}, wakeuplight: {}", alarm.getAlarmTime(), alarm.isVibrate(),
 				alarm.isWakeupLight(), alarm.getAudioStream(), alarm.getRingTone());
+
+		int length = 64;
+		WS2812.get().init(length); // init a chain of 64 LEDs
+		WS2812.get().clear();
+		if (alarm.getAlarmTime() != null) {
+			for (int i = 0; i < length; i++) {
+				WS2812.get().setPixelColor(i, Color.RED);
+			}
+		}
+		WS2812.get().show();
 	}
 }
