ab4a05dad029dd429fe3c16866c3c1c41ec3b819
Michiel Leegwater <michiel@leegwater.info>
2017-07-18 08:52:43 +0200

    Simplified gamma corrections.

--- lib.rpiws281x/src/main/java/com/externalalarmclock/lib/rpiws281x/RpiWs281x.java
+++ lib.rpiws281x/src/main/java/com/externalalarmclock/lib/rpiws281x/RpiWs281x.java
@@ -15,7 +15,7 @@ import com.externalalarmclock.rpiws281x.ws2811_t;
 import com.sun.jna.Native;
 
 public class RpiWs281x {
-	public static final List<Integer> GAMMA = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	public static final int[] GAMMA = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 			0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8,
 			8, 9, 9, 9, 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 21,
 			21, 22, 22, 23, 23, 24, 25, 25, 26, 27, 27, 28, 29, 29, 30, 31, 31, 32, 33, 34, 34, 35, 36, 37, 37, 38, 39,
@@ -25,7 +25,7 @@ public class RpiWs281x {
 			128, 129, 131, 132, 134, 135, 137, 138, 140, 142, 143, 145, 146, 148, 150, 151, 153, 155, 157, 158, 160,
 			162, 163, 165, 167, 169, 170, 172, 174, 176, 178, 179, 181, 183, 185, 187, 189, 191, 193, 194, 196, 198,
 			200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 227, 229, 231, 233, 235, 237, 239, 241,
-			244, 246, 248, 250, 252, 255);
+			244, 246, 248, 250, 252, 255};
 	private final RpiWs281xLibrary lib;
 	private final ws2811_t strip = new ws2811_t();
 	private final Map<RpiWs281xChannel, Integer> channelIndexMap = new HashMap<>();
@@ -100,10 +100,10 @@ public class RpiWs281x {
 	}
 
 	private int corectGamma(int rgb) {
-		byte correctedW = (byte) GAMMA.get((rgb & 0xFF000000) >> 24).intValue();
-		byte correctedR = (byte) GAMMA.get((rgb & 0x00FF0000) >> 16).intValue();
-		byte correctedG = (byte) GAMMA.get((rgb & 0x0000FF00) >> 8).intValue();
-		byte correctedB = (byte) GAMMA.get(rgb & 0x000000FF).intValue();
+		byte correctedW = (byte) GAMMA[(rgb & 0xFF000000) >> 24];
+		byte correctedR = (byte) GAMMA[(rgb & 0x00FF0000) >> 16];
+		byte correctedG = (byte) GAMMA[(rgb & 0x0000FF00) >> 8];
+		byte correctedB = (byte) GAMMA[rgb & 0x000000FF];
 
 		return ((correctedW << 24) & 0xFF000000) | ((correctedR << 16) & 0x00FF0000) | ((correctedG << 8) & 0x0000FF00)
 				| (correctedB & 0x000000FF);
