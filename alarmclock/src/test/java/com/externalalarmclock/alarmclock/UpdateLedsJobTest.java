0cd7c80181705a1bbf91e240fbb0b0fe1976300b
Michiel Leegwater <michiel@leegwater.info>
2017-10-20 23:26:35 +0200

    Updated to Gradle 4.2.1. Fixed pitest build.

--- alarmclock/src/test/java/com/externalalarmclock/alarmclock/UpdateLedsJobTest.java
+++ alarmclock/src/test/java/com/externalalarmclock/alarmclock/UpdateLedsJobTest.java
@@ -16,21 +16,21 @@ public class UpdateLedsJobTest {
 	private final AlarmStore alarmStore = new AlarmStore();
 	private final UpdateLedsJob sut = new UpdateLedsJob(LoggerFactory.getLogger(UpdateLedsJob.class), alarmStore);
 	private final IRpiWs281x device = Mockito.mock(IRpiWs281x.class);
-	
+
 	@Before
 	public void setUp() {
 		sut.setDevice(device);
-		
+
 		RpiWs281xChannel channel = new RpiWs281xChannel();
-		Mockito.when(device.getChannels()).thenReturn(Arrays.asList(channel ));
+		Mockito.when(device.getChannels()).thenReturn(Arrays.asList(channel));
 	}
-	
+
 	@Test
 	public void testUpdateLedsNoAlarm() throws JobExecutionException {
 		alarmStore.setNextAlarm(null);
-		
+
 		sut.doJob(null);
-		
+
 		Mockito.verify(device).getChannels();
 		Mockito.verify(device, Mockito.never()).render(Mockito.any());
 	}
