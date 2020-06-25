011cc20e9fa36637e326cd28a7eb1f783a16f866
Michiel Leegwater <mleegwt@users.noreply.github.com>
2020-06-25 18:25:40 +0200

    Trying to fix SetNextAlarmResourceTest without IDE

--- alarmclock/src/test/kotlin/SetNextAlarmResourceTest.kt
+++ alarmclock/src/test/kotlin/SetNextAlarmResourceTest.kt
@@ -9,12 +9,15 @@ import com.externalalarmclock.alarmclock.AlarmStore
 import com.externalalarmclock.lib.rpiws281x.RpiWs281xChannel
 import com.externalalarmclock.pojo.ExternalAlarm
 
+import java.time.Duration
+
 class SetNextAlarmResourceTest {
     private val alarmStore = AlarmStore()
     private val resource = SetNextAlarmResource(alarmStore)
     private val channel = RpiWs281xChannel()
     @Test
     fun setNextAlarmTest() {
+        alarmStore.wakeUpLightDuration = Duration.ofMinutes(30);
         alarmStore.addChannels(Arrays.asList(channel))
         val alarm = ExternalAlarm()
         resource.setNextAlarm(alarm)
